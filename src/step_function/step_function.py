1089bf3c42243e79b0bcfa3ef81f120f842b7891
zyangbi <62045232+zyangbi@users.noreply.github.com>
2024-04-05 03:10:01 -0700

    create workflow and states from a step function definition

--- src/step_function/step_function.py
+++ src/step_function/step_function.py
@@ -1,4 +1,8 @@
-from os import stat_result
+import boto3
+import json
+from typing import Any
+
+from attr import define
 
 
 class State:
@@ -15,8 +19,9 @@ class State:
 class Task(State):
     """Task state in Step Function."""
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, arn: str):
         super().__init__(name)
+        self.arn = arn
 
 
 class Parallel(State):
@@ -50,3 +55,61 @@ class Workflow:
     def get_execution_time(self) -> float:
         total_time = sum(state.get_execution_time() for state in self.states)
         return total_time
+
+
+def create_state(name, state_def: dict) -> State:
+    """Create a State object from a state definition."""
+    if state_def["Type"] == "Task":
+        arn = state_def["Parameters"]["FunctionName"]
+        return Task(name, arn)
+
+    elif state_def["Type"] == "Parallel":
+        parallel = Parallel(name)
+        for branch_def in state_def["Branches"]:
+            branch = create_workflow(branch_def)
+            parallel.add_branch(branch)
+        return parallel
+
+    else:
+        return State(name)  ## should throw an exception
+
+
+def create_workflow(workflow_def: dict) -> Workflow:
+    """Create a Workflow object from a workflow definition."""
+    workflow = Workflow()
+
+    state_name = workflow_def["StartAt"]  # starting state
+    while 1:
+        # add state to workflow
+        state_def = workflow_def["States"][state_name]
+        workflow.add_state(create_state(state_name, state_def))
+
+        # go to next state
+        if "Next" in state_def:
+            state_name = state_def["Next"]
+        elif "End" in state_def:
+            break  # end of workflow
+        else:
+            break  ## should throw an exception
+
+    return workflow
+
+
+def get_step_func_def(arn: str) -> dict:
+    """Get a step function's definition."""
+    try:
+        client = boto3.client("stepfunctions")
+        response = client.describe_state_machine(stateMachineArn=arn)
+        definition = json.loads(response["definition"])
+        return definition
+    except Exception as e:
+        print(f"Error retrieving state machine: {e}")
+        return []
+
+
+arn = "arn:aws:states:us-west-2:898429789601:stateMachine:RegressionTuning"
+step_function_def = get_step_func_def(arn)
+workflow = create_workflow(step_function_def)
+
+
+print(workflow)
